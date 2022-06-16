ee53e6a6da164535deccc951b5b26963a3e3146c
rmaSans <69686977+rmaSans@users.noreply.github.com>
2022-06-16 20:26:32 +0800

    Add upload button and some CSS

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -8,6 +8,6 @@ def create_app():
     from .auth import auth
     
     app.register_blueprint(views,url_prefix='/')
-    app.register_blueprint(auth,url_prefix='/')
+    # app.register_blueprint(auth,url_prefix='/')
 
     return app
