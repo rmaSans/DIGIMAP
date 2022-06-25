e9ebc19a4ae87d32d4c4c0f8669a4787d838d138
Tongki Ilagan <ilagan.tongki@gmail.com>
2022-06-26 05:28:48 +0800

    Added an image upload feature
    uploaded images will be located at webapp -> static -> img -> uploads

--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,15 @@
 from webapp import digiapp
-from flask import render_template
+from flask import render_template, request, redirect
+import os
 
-
-@digiapp.route('/')
+digiapp.config["IMAGE_UPLOADS"] = "F:\\Users\\Tongki\\Desktop\\Term 2\\DIGIMAP\\Project\\DIGIMAP\\webapp\\static\\img\\uploads"
+@digiapp.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+      if request.files:
+        image = request.files["image"]
+        image.save(os.path.join(digiapp.config["IMAGE_UPLOADS"], image.filename))
+        return redirect(request.url)
     return render_template('index.html')
 
 @digiapp.route('/mechanisms')
@@ -13,3 +19,12 @@ def about():
 @digiapp.route('/team')
 def team():
     return render_template('team.html')
+
+#@digiapp.route('/upload-image', )
+# def upload_image():
+#     if request.method == "POST":
+#         if request.files:
+#           image = request.files["image"]
+#           image.save(os.path.join(digiapp.config["IMAGE_UPLOADS"], image.filename))
+#           return redirect(request.url)
+    
